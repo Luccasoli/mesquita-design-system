f22e84c6d9d9d64699c7359256046cd4609acc69
Lucas Mesquita <lucasoliveira98@outlook.com>
2024-04-14 19:57:43 -0300

    chore: study basic of bundle

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/basic/main.js",
+  output: [
+    {
+      file: "bundle.js",
+      format: "esm",
+    },
+    {
+      file: "bundle.min.js",
+      format: "iife",
+      name: "version",
+      plugins: [terser()],
+    },
+  ],
+  plugins: [json()],
+};
